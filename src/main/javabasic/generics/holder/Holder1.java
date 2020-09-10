194f30c2f798a5999d5a3a4f19b9929c1895873b
LemoonCan <lemoon.can@gmail.com>
2020-09-10 23:06:06 +0800

    feat: 🎸 泛型应用

---
+++ src/main/javabasic/generics/holder/Holder1.java
@@ -0,0 +1,22 @@
+package main.javabasic.generics.holder;
+
+/**
+ * @author lee
+ * @date 2020-09-10
+ *
+ * 无法持有任何其他类型的对象
+ */
+public class Holder1 {
+    private Automobile automobile;
+
+    public Holder1(Automobile automobile) {
+        this.automobile = automobile;
+    }
+
+    Automobile get(){
+        return automobile;
+    }
+
+}
+
+class Automobile{}
