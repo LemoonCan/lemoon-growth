247b704a402770a2ff5b6f5189836af72559a4e9
LemoonCan <lemoon.can@gmail.com>
2020-11-12 22:08:25 +0800

    feat: 🎸 并发问题-共享资源竞争

---
+++ src/main/javabasic/multithread/share/EventChecker.java
@@ -0,0 +1,42 @@
+package main.javabasic.multithread.share;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author lee
+ * @date 2020-11-12
+ */
+public class EventChecker implements Runnable {
+    private IntGenerator intGenerator;
+    private final int id;
+
+    public EventChecker(IntGenerator generator, int id) {
+        this.intGenerator = generator;
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+        while (!intGenerator.isCanceled()) {
+            int val = intGenerator.next();
+            if (val % 2 != 0) {
+                System.out.println("线程" + id + "执行获得的" + val + " not even!");
+                intGenerator.cancel();
+            }
+        }
+    }
+
+    public static void test(IntGenerator gp, int count) {
+        System.out.println("Press Control-c to exit");
+        ExecutorService exec = Executors.newCachedThreadPool();
+        for (int i = 0; i < count; i++) {
+            exec.execute(new EventChecker(gp, i));
+        }
+        exec.shutdown();
+    }
+
+    public static void test(IntGenerator gp) {
+        test(gp, 10);
+    }
+}
