bc05856050f56599f1bda875a0a85004d262b29e
LemoonCan <lemoon.can@gmail.com>
2021-03-31 22:40:50 +0800

    feat: 🎸 Java NIO

---
+++ src/main/javabasic/datatype/BinaryTest.java
@@ -0,0 +1,12 @@
+package main.javabasic.datatype;
+
+/**
+ * @author lee
+ * @date 3/12/21
+ */
+public class BinaryTest {
+    public static void main(String[] args) {
+        System.out.println(Integer.toBinaryString(-1));
+        System.out.println(Integer.toBinaryString(-1<<29));
+    }
+}
---
+++ src/main/javabasic/multithread/communication/newcomponent/LockSupportDemo.java
@@ -0,0 +1,26 @@
+package main.javabasic.multithread.communication.newcomponent;
+
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * @author lee
+ * @date 3/11/21
+ */
+public class LockSupportDemo {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(()->{
+            System.out.println("children thread begin park!");
+            LockSupport.park();
+            System.out.println("children thread end park!");
+        });
+        t.start();
+        Thread.sleep(10000);
+        System.out.println("main thread begin unpark!");
+        LockSupport.unpark(t);
+
+        SynchronousQueue synchronousQueue = new SynchronousQueue<Runnable>();
+        synchronousQueue.offer(t);
+        new Thread();
+    }
+}
