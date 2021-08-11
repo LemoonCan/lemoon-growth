7f5bed794abcf5677ce4518653ee5927eb600d69
LemoonCan <lemoon.can@gmail.com>
2021-08-11 23:36:31 +0800

    feat: 🎸 assert 测试

---
+++ unit-demo/src/main/java/javabasic/judge/AssertDemo.java
@@ -0,0 +1,19 @@
+package javabasic.judge;
+
+/**
+ * @author lee
+ * @date 8/10/21
+ * 默认不启动断言检查，一般用于开发测试，是一种调试方式
+ * 若开启断言检查(vm options添加参数 -ea 或 -enableassertions)，断言为false会抛出AssertionError
+ */
+public class AssertDemo {
+    public static void main(String[] args) {
+        try {
+            String s = "test";
+            assert s == null;
+            System.out.println("执行");
+        } catch (Exception e){
+            System.err.println(e);
+        }
+    }
+}
