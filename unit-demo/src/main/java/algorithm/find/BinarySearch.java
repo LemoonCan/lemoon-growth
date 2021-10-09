c78af0d2343f518bf1e287f2135fb6b66666ffbc
LemoonCan <lemoon.can@gmail.com>
2021-10-09 21:31:32 +0800

    feat: 🎸 算法之二分查找

---
+++ unit-demo/src/main/java/algorithm/find/BinarySearch.java
@@ -0,0 +1,28 @@
+package algorithm.find;
+
+/**
+ * @author lee
+ * @date 2021/10/9
+ */
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        System.out.println(search(nums, 9));
+        System.out.println(search(nums, 2));
+    }
+
+    public static int search(int[] data, int target) {
+        int left = 0, right = data.length - 1, mid;
+        while (left <= right) {
+            mid = (right + left) / 2;
+            if (target == data[mid]) {
+                return mid;
+            } else if (target > data[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
