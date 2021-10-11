6a53da11f7ece0c841d0fece3a0af96d9edf3ca1
LemoonCan <lemoon.can@gmail.com>
2021-10-11 23:17:31 +0800

    feat: 🎸 旋转数组之根据移动规律推出的解法
    但不明白为什么是这个规律

--- unit-demo/src/main/java/algorithm/doublepointer/RotateArray.java
+++ unit-demo/src/main/java/algorithm/doublepointer/RotateArray.java
@@ -16,13 +16,22 @@ import java.util.Arrays;
  */
 public class RotateArray {
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5, 6, 7};
-        rotate(nums, 5);
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        rotate(nums, 8);
         System.out.println(Arrays.toString(nums));
 
         int[] nums2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27};
-        rotate2(nums2, 11);
+        rotate2(nums2, 12);
         System.out.println(Arrays.toString(nums2));
+
+        int[] numsx1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27};
+        rotatex1(numsx1, 13);
+        System.out.println(Arrays.toString(numsx1));
+
+        int[] numsx2 = {1,2,3,4,5,6,7};
+        rotatex2(numsx2, 4);
+        System.out.println(Arrays.toString(numsx2));
+
     }
 
     /**
@@ -30,6 +39,22 @@ public class RotateArray {
      * 若操作的数组长度➗移动位置=倍数...余数
      * 余数不等于0，继续分组交换，移动数=原数组长度-原数组长度%原移动数；操作数组长度=原移动数；
      * 余数等于0，结束循环。
+     * <p>
+     * 替换完最后一位数后另做考虑
+     * <p>
+     * 举例 7位数，向右移动3位
+     * 1,2,3,4,5,6,7
+     * 1.每3位替换
+     * 4,2,3,1,5,6,7
+     * 4,5,3,1,2,6,7
+     * 4,5,6,1,2,3,7
+     * 7,5,6,1,2,3,4
+     * <p>
+     * 2.前3位，向右移动2位 2=3-7%3
+     * 6,5,7,1,2,3,4
+     * <p>
+     * 3.前2位，向右移动1位 1=2-3%2
+     * 5,6,7,1,2,3,4
      *
      * @param nums
      * @param k
@@ -55,30 +80,26 @@ public class RotateArray {
         }
     }
 
-
+    /**
+     * 环状替换
+     *
+     * @param nums
+     * @param k
+     */
     public static void rotate2(int[] nums, int k) {
         int step = k % nums.length;
         if (step <= 0) return;
-        int i = 0;
-        int base;
-        while ((base = i * step) < nums.length) {
-            for (int j = 0; j < step; j++) {
-                int movePosition = (base + j) % nums.length;
-                int temp = nums[j];
-                nums[j] = nums[movePosition];
-                nums[movePosition] = temp;
-            }
-            i++;
-        }
-    }
 
-    public static void rotate3(int[] nums, int k) {
-        int commonDivisor = maxCommonDivisor(nums.length, k);
-        for(int i=0;i<=commonDivisor;i++){
-            int start = i;
-            while ((start+k)%nums.length!=i) {
-                int temp = nums[start + k];
+        int commonDivisor = maxCommonDivisor(nums.length, step);
+        for (int i = 0; i < commonDivisor; i++) {
+            int movePosition = i;
+            int temp = nums[i];
+            while ((movePosition = (movePosition + step) % nums.length) != i) {
+                int next = nums[movePosition];
+                nums[movePosition] = temp;
+                temp = next;
             }
+            nums[i] = temp;
         }
     }
 
@@ -104,5 +125,54 @@ public class RotateArray {
         }
         // 返回最大公约数
         return n;
+    }
+
+    /**
+     * @param nums
+     * @param k
+     */
+    public static void rotatex1(int[] nums, int k) {
+        int step = k % nums.length;
+        if (step <= 0) return;
+        int i = 0;
+        int base;
+        while ((base = i * step) < nums.length) {
+            for (int j = 0; j < step; j++) {
+                int movePosition = (base + j) % nums.length;
+                int temp = nums[j];
+                nums[j] = nums[movePosition];
+                nums[movePosition] = temp;
+            }
+            i++;
+        }
+    }
+
+    public static void rotatex2(int[] nums, int k) {
+        int step = k % nums.length;
+        int first = 0;
+        int length = nums.length;
+        while (step > 0) {
+            int base;
+            int i = 1;
+            while ((base = i * step) < length) {
+                for (int j = 0; j < step; j++) {
+                    int movePosition = first+(base + j) % length;
+                    int temp = nums[movePosition];
+                    nums[movePosition] = nums[first+j];
+                    nums[first+j] = temp;
+                }
+                i++;
+            }
+
+            int remainder = length % step;
+            int move = step - remainder;
+            if(move>=remainder || remainder==0){
+                step = 0;
+            }else {
+                first = first + step - remainder;
+                length = remainder;
+                step = 1;
+            }
+        }
     }
 }
