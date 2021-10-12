62e812abbc8b2593080380c19783791b33395efb
LemoonCan <lemoon.can@gmail.com>
2021-10-12 22:58:08 +0800

    feat: 🎸 双指针解法练习2
    移动0至右边及找递增数组中相加等于目标值的两数

---
+++ unit-demo/src/main/java/algorithm/doublepointer/MoveZeros.java
@@ -0,0 +1,47 @@
+package algorithm.doublepointer;
+
+/**
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ * 示例:
+ * 输入: [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ *
+ * @author lee
+ * @date 2021/10/12
+ */
+public class MoveZeros {
+    public void moveZeroes(int[] nums) {
+        /**
+         * 从左至右遍历非0,与第一个非0交换
+         */
+        int left = 0;
+        /**
+         * 右边第一个0
+         */
+        int right = 0;
+
+        while (left < nums.length) {
+            if (nums[left] != 0) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                right++;
+            }
+            left++;
+        }
+    }
+
+    public void moveZeroes2(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                count++;
+            } else {
+                nums[i - count] = nums[i];
+            }
+        }
+        for (int i = 1; i <= count; i++) {
+            nums[nums.length - i] = 0;
+        }
+    }
+}
---
+++ unit-demo/src/main/java/algorithm/doublepointer/TwoSum.java
@@ -0,0 +1,32 @@
+package algorithm.doublepointer;
+
+/**
+ * 递增数组，从数组中找出两个数满足相加之和等于target
+ * 对应唯一答案
+ * <p>
+ * 输入：numbers = [2,7,11,15], target = 9
+ * 输出：[1,2]
+ *
+ * @author lee
+ * @date 2021/10/12
+ */
+public class TwoSum {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[]{left + 1, right + 1};
+            }
+            if (sum > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+
+        return null;
+    }
+}
