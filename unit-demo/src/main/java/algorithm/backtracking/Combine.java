8eefb5e5a8059d142256868c9544edb6bed6ad23
LemoonCan <lemoon.can@gmail.com>
2021-10-27 22:12:34 +0800

    feat: 🎸 算法之回溯解法(递归、深度优先遍历)
    组合及全排列

--- unit-demo/src/main/java/algorithm/backtracking/Combine.java
+++ unit-demo/src/main/java/algorithm/backtracking/Combine.java
@@ -1,30 +1,34 @@
-package algorithm.array;
+package algorithm.backtracking;
 
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
  * https://leetcode-cn.com/problems/combinations/
  * 组合
+ *
  * @author lee
  * @date 2021/10/26
  */
 public class Combine {
     public static void main(String[] args) {
         System.out.println(combine(5, 4));
+        System.out.println(combine2(5, 4));
     }
 
     public static List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> combine = new ArrayList<>();
         for (int i = 1; i <= n - k + 1; i++) {
-            if(k==1){
+            if (k == 1) {
                 List<Integer> item = new ArrayList<>();
                 item.add(i);
                 combine.add(item);
                 continue;
             }
-            if(k==2){
-                for (int j = i + 1; j <= n; j++){
+            if (k == 2) {
+                for (int j = i + 1; j <= n; j++) {
                     List<Integer> item = new ArrayList<>();
                     item.add(i);
                     item.add(j);
@@ -47,5 +51,32 @@ public class Combine {
 
         }
         return combine;
+    }
+
+    public static List<List<Integer>> combine2(int n, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(n, k, 1, new LinkedList<>(), res);
+        return res;
+    }
+
+    /**
+     * 1 2 3 4 5   (3)
+     *选        1                       2         3
+     *      (2,3,4,5)                (3,4,5)    (4,5)
+     *选    2     3     4
+     *  (3,4,5) (4,5)  5
+     */
+    static void dfs(int n, int k, int begin, Deque<Integer> path, List<List<Integer>> res) {
+        if (path.size() == k) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        //5 3 path.size=1 5-2=3 4
+        for (int i = begin; i <= n - (k - path.size()) + 1; i++) {
+            path.add(i);
+            dfs(n, k, i + 1, path, res);
+            path.removeLast();
+        }
     }
 }
---
+++ unit-demo/src/main/java/algorithm/backtracking/Permute.java
@@ -0,0 +1,69 @@
+package algorithm.backtracking;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * https://leetcode-cn.com/problems/permutations/submissions/
+ * 全排列
+ * @author lee
+ * @date 2021/10/27
+ */
+public class Permute {
+    public static void main(String[] args) {
+        System.out.println(permute(new int[]{1,2,3}));
+        System.out.println(permute2(new int[]{1,2,3}));
+    }
+
+    public static List<List<Integer>> permute(int[] nums) {
+        Deque<Integer> set = new LinkedList<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        permute(res,set,new LinkedList<>());
+        return res;
+    }
+
+    static void permute(List<List<Integer>> res,Deque<Integer> nums,Deque<Integer> item){
+        if(nums.size()==1){
+            item.add(nums.poll());
+            res.add(new LinkedList<>(item));
+            item.removeLast();
+            return;
+        }
+        for (Integer num : nums) {
+            item.add(num);
+            LinkedList<Integer> truncateNum = new LinkedList<>(nums);
+            truncateNum.remove(num);
+            permute(res,truncateNum,item);
+            item.removeLast();
+        }
+    }
+
+    public static List<List<Integer>> permute2(int[] nums) {
+        List<List<Integer>> res = new LinkedList<>();
+        dfs(nums,0,res);
+        return res;
+    }
+
+    static void dfs(int[] nums,int begin,List<List<Integer>> res){
+        if(nums.length==begin){
+            res.add(Arrays.stream(nums)
+                    .boxed()
+                    .collect(Collectors.toList()));
+        }
+        for (int i = begin; i < nums.length; i++) {
+            swap(nums,begin,i);
+            dfs(nums,begin+1,res);
+            swap(nums,begin,i);
+        }
+    }
+    static void swap(int[] nums,int i,int j){
+        int temp = nums[j];
+        nums[j] = nums[i];
+        nums[i] = temp;
+    }
+
+
+}
