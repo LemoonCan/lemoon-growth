cd642e86af032a61b27041c564adb1204f0089be
LemoonCan <lemoon.can@gmail.com>
2021-11-09 15:12:03 +0800

    feat: 🎸 算法之双指针解法
    复习三数之和及比较包含退格的字符串

--- unit-demo/src/main/java/algorithm/doublepointer/BackspaceCompare.java
+++ unit-demo/src/main/java/algorithm/doublepointer/BackspaceCompare.java
@@ -3,6 +3,7 @@ package algorithm.doublepointer;
 /**
  * 比较含退格的字符串
  * https://leetcode-cn.com/problems/backspace-string-compare/
+ *
  * @author lee
  * @date 2021/11/8
  */
@@ -11,17 +12,25 @@ public class BackspaceCompare {
         String s = "ab#c", t = "ad#c";
         System.out.println(backspaceCompare(s, t));
 
-        s = "ab##"; t = "c#d#";
+        s = "ab##";
+        t = "c#d#";
         System.out.println(backspaceCompare(s, t));
 
-        s = "a##c"; t = "#a#c";
+        s = "a##c";
+        t = "#a#c";
         System.out.println(backspaceCompare(s, t));
 
-        s = "a#c"; t = "b";
+        s = "a#c";
+        t = "b";
         System.out.println(backspaceCompare(s, t));
 
-        s="y#fo##f";t="y#f#o##f";
+        s = "y#fo##f";
+        t = "y#f#o##f";
         System.out.println(backspaceCompare(s, t));
+
+//        System.out.println(backspaceCompare2("y#fo##f", "y#f#o##f"));
+        System.out.println(backspaceCompare2("a##c", "#a#c"));
+        System.out.println(backspaceCompare3("a##c", "#a#c"));
     }
 
     public static boolean backspaceCompare(String s, String t) {
@@ -43,7 +52,7 @@ public class BackspaceCompare {
         return true;
     }
 
-    public static String backCompelete(String s){
+    public static String backCompelete(String s) {
         char[] cs = s.toCharArray();
         int size = cs.length;
         int i = 0;
@@ -65,8 +74,115 @@ public class BackspaceCompare {
                 i++;
             }
         }
-        return new String(cs,0,size);
+        return new String(cs, 0, size);
     }
 
     //逆序遍历
+    public static boolean backspaceCompare2(String s, String t) {
+        int sSkip = 0, tSkip = 0;
+        int p1 = s.length() - 1, p2 = t.length() - 1;
+        while (p1 >= 0 && p2 >= 0) {
+            if (s.charAt(p1) == '#') {
+                sSkip++;
+                p1--;
+                continue;
+            }
+            if (t.charAt(p2) == '#') {
+                tSkip++;
+                p2--;
+                continue;
+            }
+            if (sSkip == 0 && tSkip == 0) {
+                if (s.charAt(p1) != t.charAt(p2)) {
+                    return false;
+                }
+                p1--;
+                p2--;
+            } else {
+                if (sSkip > 0) {
+                    sSkip--;
+                    p1--;
+                }
+                if (tSkip > 0) {
+                    tSkip--;
+                    p2--;
+                }
+            }
+        }
+
+        while (p1 >= 0) {
+            if (s.charAt(p1) == '#') {
+                sSkip++;
+                p1--;
+                continue;
+            }
+            if (sSkip > 0) {
+                p1--;
+                sSkip--;
+            } else {
+                return false;
+            }
+        }
+
+        while (p2 >= 0) {
+            if (t.charAt(p2) == '#') {
+                tSkip++;
+                p2--;
+                continue;
+            }
+            if (tSkip > 0) {
+                p2--;
+                sSkip--;
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean backspaceCompare3(String s, String t) {
+        int sSkip = 0, tSkip = 0;
+        int sp = s.length() - 1, tp = t.length() - 1;
+
+        while (sp >= 0 || tp >= 0) {
+            while (sp >= 0) {
+                if (s.charAt(sp) == '#') {
+                    sSkip++;
+                    sp--;
+                } else if (sSkip > 0) {
+                    sSkip--;
+                    sp--;
+                } else {
+                    break;
+                }
+            }
+
+            while (tp >= 0) {
+                if (t.charAt(tp) == '#') {
+                    tSkip++;
+                    tp--;
+                } else if (tSkip > 0) {
+                    tSkip--;
+                    tp--;
+                } else {
+                    break;
+                }
+            }
+
+
+            if (sp >= 0 && tp >= 0) {
+                if (s.charAt(sp) != t.charAt(tp)) {
+                    return false;
+                }
+            } else {
+                if (sp >= 0 || tp >= 0) {
+                    return false;
+                }
+            }
+            sp--;
+            tp--;
+        }
+        return true;
+    }
 }
