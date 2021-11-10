a85ac9d012ecd673f227c2c1471f445eb8fad4f8
LemoonCan <lemoon.can@gmail.com>
2021-11-10 15:45:07 +0800

    feat: 🎸 算法之双指针解法
    区间列表的交集及盛最多水的容器

---
+++ unit-demo/src/main/java/algorithm/doublepointer/InterverIntersection.java
@@ -0,0 +1,52 @@
+package algorithm.doublepointer;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 区间列表的交集
+ * https://leetcode-cn.com/problems/interval-list-intersections/
+ *
+ * @author lee
+ * @date 2021/11/10
+ */
+public class InterverIntersection {
+    public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
+        List<int[]> res = new LinkedList<>();
+        int p1 = 0, p2 = 0;
+        while (p1 < firstList.length && p2 < secondList.length) {
+            if (firstList[p1][1] < secondList[p2][0]) {
+                p1++;
+            } else if (firstList[p1][0] > secondList[p2][1]) {
+                p2++;
+            } else {
+                int[] item = {Math.max(firstList[p1][0], secondList[p2][0]), Math.min(firstList[p1][1], secondList[p2][1])};
+                res.add(item);
+                if (firstList[p1][1] > secondList[p2][1]) {
+                    p2++;
+                } else {
+                    p1++;
+                }
+            }
+        }
+        return res.toArray(new int[res.size()][]);
+    }
+
+    public int[][] intervalIntersection2(int[][] firstList, int[][] secondList) {
+        List<int[]> res = new LinkedList<>();
+        int p1 = 0, p2 = 0;
+        while (p1 < firstList.length && p2 < secondList.length) {
+            int low = Math.max(firstList[p1][0], secondList[p2][0]);
+            int high = Math.min(firstList[p1][1], secondList[p2][1]);
+            if(low<=high){
+                res.add(new int[]{low,high});
+            }
+            if(firstList[p1][1]>secondList[p2][1]){
+                p2++;
+            }else{
+                p1++;
+            }
+        }
+        return res.toArray(new int[res.size()][]);
+    }
+}
---
+++ unit-demo/src/main/java/algorithm/doublepointer/MaxArea.java
@@ -0,0 +1,27 @@
+package algorithm.doublepointer;
+
+/**
+ * 盛最多水的容器
+ * https://leetcode-cn.com/problems/container-with-most-water/
+ *
+ * @author lee
+ * @date 2021/11/10
+ */
+public class MaxArea {
+    public int maxArea(int[] height) {
+        if (height.length < 2) {
+            return 0;
+        }
+        int max = 0;
+        int left = 0, right = height.length;
+        while (left < right) {
+            max = Math.max(max, (right - left) * Math.min(height[left], height[right]));
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return max;
+    }
+}
