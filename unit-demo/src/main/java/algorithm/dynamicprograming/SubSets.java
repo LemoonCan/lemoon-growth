fce90768fd7bfbf2b216fb9b178d1adb6c084849
LemoonCan <lemoon.can@gmail.com>
2021-11-28 23:47:18 +0800

    feat: 🎸 算法之动态规划解法
    子集

---
+++ unit-demo/src/main/java/algorithm/dynamicprograming/SubSets.java
@@ -0,0 +1,31 @@
+package algorithm.dynamicprograming;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author lee
+ * @date 2021/11/28
+ */
+public class SubSets {
+    public static void main(String[] args) {
+        int[]  nums = {1,2,3};
+        System.out.println(subsets(nums));
+    }
+
+    public static List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<>());
+
+        for (int i = 0; i < nums.length; i++) {
+            int size = res.size();
+            for (int j = 0; j < size; j++) {
+                List<Integer> item = new ArrayList<>(res.get(j));
+                item.add(nums[i]);
+                res.add(item);
+            }
+        }
+
+        return res;
+    }
+}
