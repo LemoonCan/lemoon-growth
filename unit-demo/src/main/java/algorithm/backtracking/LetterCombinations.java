19200df81ce2fa1ab654e6f411b87bf4be99f585
LemoonCan <lemoon.can@gmail.com>
2021-12-08 14:24:28 +0800

    feat: 🎸 算法之回溯解法
    电话号码的字母组合

---
+++ unit-demo/src/main/java/algorithm/backtracking/LetterCombinations.java
@@ -0,0 +1,52 @@
+package algorithm.backtracking;
+
+import java.util.*;
+
+/**
+ * 电话号码的字母组合
+ * https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/
+ *
+ * @author lee
+ * @date 2021/12/8
+ */
+public class LetterCombinations {
+    public static void main(String[] args) {
+        String digits = "23";
+        LetterCombinations lc = new LetterCombinations();
+        System.out.println(lc.letterCombinations(digits));
+    }
+
+    private Map<Character, char[]> map = new HashMap<Character, char[]>(){{
+        put('2', new char[]{'a','b','c'});
+        put('3', new char[]{'d','e','f'});
+        put('4', new char[]{'g','h','i'});
+        put('5', new char[]{'j','k','l'});
+        put('6', new char[]{'m','n','o'});
+        put('7', new char[]{'p','q','r','s'});
+        put('8', new char[]{'t','u','v'});
+        put('9', new char[]{'w','x','y','z'});
+    }};
+
+    public List<String> letterCombinations(String digits) {
+        List<String> res = new ArrayList<>();
+        if(digits.length()<1){
+            return res;
+        }
+        dfs(digits,0,res,new StringBuilder());
+        return res;
+    }
+    
+    void dfs(String digits,int index,List<String> res,StringBuilder item){
+        if(index>=digits.length()){
+            res.add(item.toString());
+            return;
+        }
+
+        char[] input = map.get(digits.charAt(index));
+        for (int i = 0; i < input.length; i++) {
+            item.append(input[i]);
+            dfs(digits,index+1,res,item);
+            item.deleteCharAt(item.length()-1);
+        }
+    }
+}
