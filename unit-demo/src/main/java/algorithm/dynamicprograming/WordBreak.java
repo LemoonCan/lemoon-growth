c33ff3868cc0636a1fea19dec583de98a6668c5c
LemoonCan <lemoon.can@gmail.com>
2021-12-21 14:37:34 +0800

    feat: 🎸 算法之动态规划解法
    单词拆分

--- unit-demo/src/main/java/algorithm/dynamicprograming/WordBreak.java
+++ unit-demo/src/main/java/algorithm/dynamicprograming/WordBreak.java
@@ -1,4 +1,4 @@
-package algorithm.string;
+package algorithm.dynamicprograming;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -8,18 +8,25 @@ import java.util.Set;
 /**
  * @author lee
  * @date 2020-08-18
- * <p>
+ *
+ * 单词拆分
+ * https://leetcode-cn.com/problems/word-break/
  * 给定一个非空字符串 s 和一个包含非空单词列表的字典 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
  * 说明：
  * 拆分时可以重复使用字典中的单词。
  * 你可以假设字典中没有重复的单词。
+ *
  */
 public class WordBreak {
     public static void main(String[] args) {
-        String s = "applepenapple";
+        WordBreak wordBreak = new WordBreak();
+
+        String s = "leetcode";
+        System.out.println(wordBreak.wordBreak(s, Arrays.asList("leet", "code")));
+
+        s = "applepenapple";
         List<String> wordDict = Arrays.asList("apple", "pen");
 
-        WordBreak wordBreak = new WordBreak();
         boolean flag = wordBreak.wordBreak(s, wordDict);
         System.out.println(flag);
 
@@ -40,41 +47,44 @@ public class WordBreak {
      * @param wordDict
      * @return
      */
+    int max = 0;
+    Set<String> wordSet = new HashSet<>();
     public boolean wordBreak(String s, List<String> wordDict) {
-        return wordBreak(s, 0, new HashSet<>(wordDict));
+        for (int i = 0; i < wordDict.size(); i++) {
+            if (wordDict.get(i).length() > max) {
+                max = wordDict.get(i).length();
+            }
+            wordSet.add(wordDict.get(i));
+        }
+
+        return dfs(s, 0);
     }
 
-    public boolean wordBreak(String s, int begin, HashSet<String> wordDict) {
-        if (begin >= s.length()) return true;
-        for (int i = begin; i < s.length(); i++) {
-            if (wordDict.contains(s.substring(begin, i + 1))) {
-                if (wordBreak(s, i + 1, wordDict)) return true;
+    public boolean dfs(String s, int index) {
+        if (index == s.length()) {
+            return true;
+        }
+
+        int end = index + max;
+        for (int i = index + 1; i <= end && i <= s.length(); i++) {
+            if (wordSet.contains(s.substring(index, i))) {
+                if (dfs(s, i)) {
+                    return true;
+                }
             }
         }
         return false;
     }
 
-    public boolean wordBreak2(String s, List<String> wordDict){
-        boolean[] dep = new boolean[s.length()+1];
-        dep[0] = true;
-        for (int i = 1; i <= s.length(); i++) {
-            for (int j=0;j<i;j++){
-                if(dep[j]&&wordDict.contains(s.substring(j,i))){
-                    dep[i]=true;
-                    break;
-                }
-            }
-        }
-        return dep[s.length()];
-    }
-
     /**
-     * TODO ????
+     * 动态规划
+     * s[i]是否可拆分等价于 s[0~i-x] 及 s[i-x~i]是否可拆分
+     *
      * @param s
      * @param wordDict
      * @return
      */
-    public boolean wordBreak3(String s, List<String> wordDict) {
+    public boolean wordBreak2(String s, List<String> wordDict) {
         Set<String> wordDictSet = new HashSet(wordDict);
         int len = 0;
         for(String str : wordDict){
@@ -88,7 +98,6 @@ public class WordBreak {
                 if(i - j > len) j = i - len;
                 if (dp[j] && wordDictSet.contains(s.substring(j, i))) {
                     dp[i] = true;
-
                     break;
                 }
             }
