8ef2c647e74992f5fb78224c979218391590bfea
LemoonCan <lemoon.can@gmail.com>
2021-12-27 13:36:37 +0800

    feat: 🎸 算法之动态规划解法
    零钱兑换

---
+++ unit-demo/src/main/java/algorithm/backtracking/CoinChange.java
@@ -0,0 +1,69 @@
+package algorithm.backtracking;
+
+import java.util.Arrays;
+
+/**
+ * 零钱兑换
+ * https://leetcode-cn.com/problems/coin-change/
+ *
+ * @author lee
+ * @date 2021/12/26
+ */
+public class CoinChange {
+    public static void main(String[] args) {
+        int[] coins = {1, 2, 5};
+        CoinChange demo = new CoinChange();
+        System.out.println(demo.coinChange(coins, 11));
+
+        int[] coins2 = {411,412,413,414,415,416,417,418,419,420,421,422};
+        System.out.println(demo.coinChange2(coins2, 9864));
+
+    }
+
+    public int coinChange(int[] coins, int amount) {
+        Arrays.sort(coins);
+        return dfs(coins, coins.length - 1, amount);
+    }
+
+    public int dfs(int[] coins, int index, int amount) {
+        if (index < 0 || amount <= 0) {
+            return -1;
+        }
+
+        int max = amount / coins[index];
+        int min = -1;
+        for (int i = max; i >= 0; i--) {
+            int remain = amount - i * coins[index];
+            if (remain == 0) {
+                if (min == -1 || i < min) {
+                    min = i;
+                }
+            } else {
+                int remainCount = dfs(coins, index - 1, remain);
+                if (remainCount != -1) {
+                    int count = i + remainCount;
+                    if (min == -1 || count < min) {
+                        min = count;
+                    }
+                }
+            }
+        }
+
+        return min;
+    }
+
+    public int coinChange2(int[] coins, int amount) {
+        int max = amount + 1;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, max);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
--- unit-demo/src/main/java/algorithm/dynamicprograming/MinDistance.java
+++ unit-demo/src/main/java/algorithm/dynamicprograming/MinDistance.java
@@ -10,22 +10,22 @@ package algorithm.dynamicprograming;
 public class MinDistance {
     public static void main(String[] args) {
         MinDistance demo = new MinDistance();
-        System.out.println(demo.minDistance("sea","eat"));
+        System.out.println(demo.minDistance("sea", "eat"));
     }
 
     public int minDistance(String word1, String word2) {
         int dp[][] = new int[word1.length() + 1][word2.length() + 1];
         for (int i = 0; i < word1.length(); i++) {
             for (int j = 0; j < word2.length(); j++) {
-                if(word1.charAt(i)==word2.charAt(j)){
-                    dp[i+1][j+1] = dp[i][j]+1;
-                }else {
-                    dp[i+1][j+1] = Math.max(dp[i][j+1],dp[i+1][j]);
+                if (word1.charAt(i) == word2.charAt(j)) {
+                    dp[i + 1][j + 1] = dp[i][j] + 1;
+                } else {
+                    dp[i + 1][j + 1] = Math.max(dp[i][j + 1], dp[i + 1][j]);
                 }
             }
         }
 
         int max = dp[word1.length()][word2.length()];
-        return word1.length()+word2.length()-2*max;
+        return word1.length() + word2.length() - 2 * max;
     }
 }
