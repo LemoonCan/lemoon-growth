d39706389e44e19a8081013464d645dead4ca1e3
LemoonCan <lemoon.can@gmail.com>
2022-01-05 15:09:54 +0800

    feat: 🎸 数据结构之前缀树

---
+++ unit-demo/src/main/java/datastructure/tree/Trie.java
@@ -0,0 +1,57 @@
+package datastructure.tree;
+
+/**
+ * 前缀树
+ * https://leetcode-cn.com/problems/implement-trie-prefix-tree/
+ *
+ * @author lee
+ * @date 2022/1/5
+ */
+public class Trie {
+    Trie[] children;
+    boolean end;
+
+    public Trie() {
+        this.children = new Trie[26];
+    }
+
+    /**
+     * 26叉树
+     *
+     * @param word
+     */
+    public void insert(String word) {
+        Trie node = this;
+        for (int i = 0; i < word.length(); i++) {
+            int index = word.charAt(i) - 'a';
+
+            if (node.children[index] == null) {
+                node.children[index] = new Trie();
+            }
+
+            node = node.children[index];
+        }
+        node.end = true;
+    }
+
+    public boolean search(String word) {
+        Trie node = searchPrefix(word);
+        return node != null && node.end;
+    }
+
+    public boolean startsWith(String prefix) {
+        return searchPrefix(prefix) != null;
+    }
+
+    private Trie searchPrefix(String prefix) {
+        Trie node = this;
+        for (int i = 0; i < prefix.length(); i++) {
+            int index = prefix.charAt(i) - 'a';
+            if (node.children[index] == null) {
+                return null;
+            }
+            node = node.children[index];
+        }
+        return node;
+    }
+}
