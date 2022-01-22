2680e0b1fc13897e56636b4e370666bcb9626c2b
LemoonCan <lemoon.can@gmail.com>
2022-01-22 23:38:00 +0800

    feat: 🎸 算法之环形链表

---
+++ unit-demo/src/main/java/algorithm/collection/DetectCircle.java
@@ -0,0 +1,27 @@
+package algorithm.collection;
+
+import algorithm.ListNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 环形链表2
+ * https://leetcode-cn.com/problems/linked-list-cycle-ii/
+ * @author lee
+ * @date 2022/1/22
+ */
+public class DetectCircle {
+    public ListNode detectCycle(ListNode head) {
+        Set<ListNode> exist = new HashSet<>();
+        ListNode cur = head;
+        while (cur!=null){
+            if(exist.contains(cur)){
+                return cur;
+            }
+            exist.add(cur);
+            cur = cur.next;
+        }
+        return null;
+    }
+}
---
+++ unit-demo/src/main/java/algorithm/collection/HasCircle.java
@@ -0,0 +1,30 @@
+package algorithm.collection;
+
+import algorithm.ListNode;
+
+/**
+ * 环形链表
+ * https://leetcode-cn.com/problems/linked-list-cycle/
+ * @author lee
+ * @date 2022/1/22
+ */
+public class HasCircle {
+    public boolean hasCycle(ListNode head) {
+        if(head==null&&head.next==null){
+            return false;
+        }
+
+        ListNode slow = head,quick = head.next.next;
+        while (slow!=null&&quick!=null){
+            if(slow==quick){
+                return true;
+            }
+            slow = slow.next;
+            if(quick.next==null){
+                return false;
+            }
+            quick = quick.next.next;
+        }
+        return false;
+    }
+}
