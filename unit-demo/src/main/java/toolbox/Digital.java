4e51410d4d7ac3eeff71a257c7eb4ad87a91a938
LemoonCan <lemoon.can@gmail.com>
2022-02-09 11:28:56 +0800

    feat: 🎸 数字工具
    奇偶判断

---
+++ unit-demo/src/main/java/toolbox/Digital.java
@@ -0,0 +1,17 @@
+package toolbox;
+
+/**
+ * 数字工具
+ * @author lee
+ * @date 2022/2/9
+ */
+public class Digital {
+    /**
+     * 是否奇数
+     * @param val
+     * @return
+     */
+    public static boolean judgeOdd(int val) {
+        return (val & 1) != 0;
+    }
+}
