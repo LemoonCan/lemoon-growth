15dae860b4e88debd54d3d1aecbd90d8732c446e
LemoonCan <lemoon.can@gmail.com>
2022-02-12 17:28:59 +0800

    feat: 🎸 算法之哈希相关
    常数时间插入、删除和随机获取元素

---
+++ unit-demo/src/main/java/algorithm/hashandmapping/RandomizedSet.java
@@ -0,0 +1,46 @@
+package algorithm.hashandmapping;
+
+import java.util.*;
+
+/**
+ * 常数时间插入、删除和随机获取元素
+ * @author lee
+ * @date 2022/2/12
+ */
+public class RandomizedSet {
+    Map<Integer, Integer> dict;
+    List<Integer> list;
+    Random rand = new Random();
+
+    public RandomizedSet() {
+        dict = new HashMap();
+        list = new ArrayList();
+    }
+
+    public boolean insert(int val) {
+        if (dict.containsKey(val)) return false;
+
+        //序号
+        dict.put(val, list.size());
+        list.add(list.size(), val);
+        return true;
+    }
+
+    public boolean remove(int val) {
+        if (! dict.containsKey(val)) return false;
+
+        //移动最后一个元素 到 删除元素位置
+        int lastElement = list.get(list.size() - 1);
+        int idx = dict.get(val);
+        list.set(idx, lastElement);
+        dict.put(lastElement, idx);
+        //删除最后一个元素
+        list.remove(list.size() - 1);
+        dict.remove(val);
+        return true;
+    }
+
+    public int getRandom() {
+        return list.get(rand.nextInt(list.size()));
+    }
+}
