3c18491f17399343fd4469a23fe8c965f6a6f0af
LemoonCan <lemoon.can@gmail.com>
2022-06-25 19:07:37 +0800

    test: 💍 Dubbo SPI 测试执行方法

--- springboot-demo/src/main/java/cherry/bootstrap/Application.java
+++ springboot-demo/src/main/java/cherry/bootstrap/Application.java
@@ -2,14 +2,8 @@ package cherry.bootstrap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication
-@ComponentScan(basePackages = {"cherry"})
-@EnableAspectJAutoProxy(exposeProxy=true)
-@EnableAsync
+@SpringBootApplication(scanBasePackages = {"cherry"})
 public class Application {
 
     public static void main(String[] args) {
