896dccc819ce0708f841b7b3aacd76bf6ace65b1
LemoonCan <lemoon.can@gmail.com>
2022-10-14 15:31:23 +0800

    style: 💄 格式化

--- unit-demo/src/main/java/designpattern/behavior/strategy/StatelessStrategyFactory.java
+++ unit-demo/src/main/java/designpattern/behavior/strategy/StatelessStrategyFactory.java
@@ -5,18 +5,19 @@ import java.util.Map;
 
 /**
  * 无状态的策略
+ *
  * @author lee
  * @date 2022/10/14
  */
 public class StatelessStrategyFactory {
-    private static final Map<String,Strategy> strategys = new HashMap<>();
+    private static final Map<String, Strategy> strategys = new HashMap<>();
 
     static {
-        strategys.put("A",new ConcreteStrategyA());
-        strategys.put("B",new ConcreteStrategyB());
+        strategys.put("A", new ConcreteStrategyA());
+        strategys.put("B", new ConcreteStrategyB());
     }
 
-    public static Strategy getStrategy(String type){
+    public static Strategy getStrategy(String type) {
         return strategys.get(type);
     }
 }
