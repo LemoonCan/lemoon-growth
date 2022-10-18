ab7bb58207ea5cb4f4e9fa181c8975d835737aee
LemoonCan <lemoon.can@gmail.com>
2022-10-18 14:28:13 +0800

    docs: ✏️ 马里奥注释

--- unit-demo/src/main/java/designpattern/behavior/state/State.java
+++ unit-demo/src/main/java/designpattern/behavior/state/State.java
@@ -5,9 +5,34 @@ package designpattern.behavior.state;
  * @date 2022/10/18
  */
 public enum State {
-    SMALL(),
-    SUPER(),
-    FIRE(),
-    CAPE(),
+    /**
+     * 小马里奥
+     */
+    SMALL(0),
+    /**
+     * 超级马里奥
+     */
+    SUPER(1),
+    /**
+     * 火焰马里奥
+     */
+    FIRE(2),
+    /**
+     * 斗篷马里奥
+     */
+    CAPE(3),
     ;
+    private Integer code;
+
+    State(Integer code) {
+        this.code = code;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
 }
