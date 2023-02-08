54a5f2a6123ec66f98251489e82b0e041ab3fc66
LemoonCan <lemoon.can@gmail.com>
2023-02-08 17:56:02 +0800

    feat: 🎸 基础 API 测试

--- unit-demo/src/main/java/javabasic/BigDecimalTest.java
+++ unit-demo/src/main/java/javabasic/BigDecimalTest.java
@@ -8,6 +8,7 @@ import java.math.BigDecimal;
  */
 public class BigDecimalTest {
     public static void main(String[] args) {
+        // 浮点数使用BigDecimal时，使用String存储才可正常展示
         double x = 0.1d;
         System.out.println(new BigDecimal(x));
 
@@ -16,5 +17,12 @@ public class BigDecimalTest {
 
         System.out.println(new BigDecimal(Double.toString(0.1000000000000000055511151231257827021181583404541015625d)));
         System.out.println(new BigDecimal(Double.toString(0.1)));
+
+        // 去除无效的 0
+        double z1 = 2076.00000;
+        BigDecimal z2 = BigDecimal.valueOf(z1);
+        System.out.println(z2);
+        System.out.println(z2.toPlainString());
+        System.out.println(z2.stripTrailingZeros().toPlainString());
     }
 }
--- unit-demo/src/main/java/javabasic/multithread/forkjoin/FibonacciTest.java
+++ unit-demo/src/main/java/javabasic/multithread/forkjoin/FibonacciTest.java
@@ -14,7 +14,7 @@ public class FibonacciTest {
         ForkJoinPool forkJoinPool = new ForkJoinPool();
         System.out.println("CPU核数：" + Runtime.getRuntime().availableProcessors());
         long start = System.currentTimeMillis();
-        Fibonacci fibonacci = new Fibonacci(40);
+        Fibonacci fibonacci = new Fibonacci(6);
         Future<Integer> future = forkJoinPool.submit(fibonacci);
         System.out.println(future.get());
         long end = System.currentTimeMillis();
@@ -30,13 +30,19 @@ class Fibonacci extends RecursiveTask<Integer> {
         this.n = n;
     }
 
-    // 主要的实现逻辑都在compute()里
+    /**
+     * 实现逻辑在compute()里
+     * 区分何时返回，何时拆分任务，拆分任务返回后如何整合
+     *
+     * @return
+     */
     @Override
     protected Integer compute() {
-        // 这里先假设 n >= 0
+        // 阈值内直接计算
         if (n <= 1) {
             return n;
         } else {
+            // 阈值外进行拆分
             // f(n-1)
             Fibonacci f1 = new Fibonacci(n - 1);
             f1.fork();
