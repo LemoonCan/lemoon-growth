fcea959cbc189c10e93b2cc65113b29534645641
Lemoon Can <lemoon.can@gmail.com>
2025-03-31 19:19:27 +0800

    refactor: 💡 fork/join

--- unit-demo/src/main/java/javabasic/multithread/forkjoin/FibonacciTest.java
+++ unit-demo/src/main/java/javabasic/multithread/forkjoin/FibonacciTest.java
@@ -11,14 +11,15 @@ import java.util.concurrent.RecursiveTask;
  */
 public class FibonacciTest {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        ForkJoinPool forkJoinPool = new ForkJoinPool();
-        System.out.println("CPU核数：" + Runtime.getRuntime().availableProcessors());
-        long start = System.currentTimeMillis();
-        Fibonacci fibonacci = new Fibonacci(6);
-        Future<Integer> future = forkJoinPool.submit(fibonacci);
-        System.out.println(future.get());
-        long end = System.currentTimeMillis();
-        System.out.println(String.format("耗时：%d millis", end - start));
+        try (ForkJoinPool forkJoinPool = new ForkJoinPool()) {
+            System.out.println("CPU核数：" + Runtime.getRuntime().availableProcessors());
+            long start = System.currentTimeMillis();
+            Fibonacci fibonacci = new Fibonacci(6);
+            Future<Integer> future = forkJoinPool.submit(fibonacci);
+            System.out.println(future.get());
+            long end = System.currentTimeMillis();
+            System.out.printf("耗时：%d millis%n", end - start);
+        }
     }
 }
 
@@ -34,7 +35,7 @@ class Fibonacci extends RecursiveTask<Integer> {
      * 实现逻辑在compute()里
      * 区分何时返回，何时拆分任务，拆分任务返回后如何整合
      *
-     * @return
+     * @return f(n)
      */
     @Override
     protected Integer compute() {
@@ -45,6 +46,7 @@ class Fibonacci extends RecursiveTask<Integer> {
             // 阈值外进行拆分
             // f(n-1)
             Fibonacci f1 = new Fibonacci(n - 1);
+            //异步执行子任务
             f1.fork();
             // f(n-2)
             Fibonacci f2 = new Fibonacci(n - 2);
